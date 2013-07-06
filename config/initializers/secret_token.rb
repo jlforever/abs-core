abc13e839801a8714f7b1374844aa17ed9e079cd
Jiapeng Ji <jjp4ever@gmail.com>
2013-07-06 12:27:26 -0400

    Initial commit and adding in some essential gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbsCore::Application.config.secret_token = 'bccfcce8f00bca554e41158bb24eea1d2691fc7a53591f63004a70539b8bbaf0474f7cbd220733033c0f73cbb8bf0df4a54082f945acfb8937ef4619b17d8ae9'
