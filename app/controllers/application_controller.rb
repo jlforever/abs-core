39c2f845b71b385b23c91900432b3f80dc0ec3bf
Jiapeng Ji <jjp4ever@gmail.com>
2013-07-11 15:01:02 -0400

    header changes, use variables to store menu options, add in logo, align intro image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :loading_menu_options
+  
+  def loading_menu_options
+    @menu_options ||= ["HOME", "PROGRAMS", "REGISTRATION", "TESTIMONIAL", "CONTACT US"]
+  end
+
 end
