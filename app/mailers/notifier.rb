2eadda6126d4ac860a9c9057e1cc112a943b19ad
Jiapeng Ji <jjp4ever@gmail.com>
2013-07-25 14:30:30 -0400

    Add in test email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,7 @@
 class Notifier < ActionMailer::Base
 
-
+  def test_email
+    mail(:to => 'info@alphabetaschool.org', :from => 'ABLS Info <info@alphabetaschool.org>', :subject => 'This is a test email')
+  end
 
 end
