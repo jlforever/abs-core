e4b2f1e75f73525ba45aee7b948741b912956398
Jiapeng Ji <jjp4ever@gmail.com>
2013-07-25 14:50:14 -0400

    configure sendgrid to work with action mailer since heroku has no basic integration to send mail out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,15 @@ AbsCore::Application.configure do
   
   # send mail.
   config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable threaded mode
   # config.threadsafe!
