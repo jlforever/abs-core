e9e99c2b34a20a59398a53dd547bd478308ca3c1
Jiapeng Ji <jjp4ever@gmail.com>
2013-08-08 18:22:14 -0400

    make sure contact us page email validation is in place

--- app/controllers/contact_us_pages_controller.rb
+++ app/controllers/contact_us_pages_controller.rb
@@ -11,6 +11,10 @@ class ContactUsPagesController < ApplicationController
     end
   end
   
+  def email_format_correct?(email)
+    email.match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i).to_s.present?
+  end
+  
   def create
     # detect whether all of the required fields are present
     @valid = presence_of_all_required_fields?(params)
@@ -19,7 +23,12 @@ class ContactUsPagesController < ApplicationController
     # all of the required fields are appearing in the 
     # params list
     if @valid
-      Notifier.contact_us_email(params).deliver
+      if email_format_correct?(params['email_address'])
+        Notifier.contact_us_email(params).deliver
+      else
+        flash[:flash_alert] = "Incorrect email format!"
+        redirect_to new_contact_us_page_path
+      end
     else
       flash[:flash_alert] = "Please fill in all of the required fields!"
       redirect_to new_contact_us_page_path
